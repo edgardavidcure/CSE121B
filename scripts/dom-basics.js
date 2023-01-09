933481a59b3940e0f8a83165e9c9a7ffc39fad50
Edgar Cure <edgardavid1503@gmail.com>
2023-01-09 11:37:54 -0800

    Completed activities to interact with the DOM

---
+++ scripts/dom-basics.js
@@ -0,0 +1,8 @@
+const newParagraph = document.createElement("p");
+newParagraph.innerText = "Added with Javascript!";
+document.body.appendChild(newParagraph);
+
+const newImage = document.createElement("img");
+newImage.setAttribute("src", "https://placeimg.com/200/200/animals" );
+document.body.appendChild(newImage);
+
